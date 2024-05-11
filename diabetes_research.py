9f455a770134c205bd1f27a4bfda004e823d02b9
Meryem <71948548+MeryemEroglu@users.noreply.github.com>
2024-05-11 18:23:44 +0300

    Implemented automated hyperparameter optimization and ensemble learning

--- diabetes_research.py
+++ diabetes_research.py
@@ -30,7 +30,7 @@ from catboost import CatBoostClassifier
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 500)
 
-df = pd.read_csv("diabetes/diabetes.csv")
+df = pd.read_csv("diabetes.csv")
 
 ################################################
 # 1. Exploratory Data Analysis
@@ -415,7 +415,7 @@ check_df(X)
 # 3. Base Models
 ####################################
 
-def base_modela(X, y, scoring="roc_auc"):
+def base_models(X, y, scoring="roc_auc"):
     print("Base Models...")
     classifiers = [('LR', LogisticRegression()),
                    ('KNN', KNeighborsClassifier()),
@@ -431,3 +431,124 @@ def base_modela(X, y, scoring="roc_auc"):
         cv_results = cross_validate(classifier, X, y, cv=3, scoring=scoring)
         print(f"{scoring}: {round(cv_results['test_score'].mean(), 4)} ({name}")
 base_models(X, y, scoring="accuracy")
+
+############################################
+# 4. Automated Hyperparameter Optimization
+############################################
+
+# Define hyperparameter grids for each classifier
+knn_parameters = {"n_neighbors": range(2, 50)}
+
+cart_parameters = {"max_depth": range(1, 20),
+                   "min_samples_split": range(2, 30)}
+
+rf_parameters = {"max_depth": [8, 15, None],
+             "max_features": [5, 7, "auto"],
+             "min_samples_split": [15, 20],
+             "n_estimators": [200, 300]}
+
+xgboost_parameters = {"learning_rate": [0.1, 0.01],
+                  "max_depth": [5, 8],
+                  "n_estimators": [100, 200]}
+
+lightgbm_parameters = {"learning_rate": [0.01, 0.1],
+                   "n_estimators": [300, 500]}
+
+# Define a list of classifiers with their respective hyperparameter grids
+classifiers = [('KNN', KNeighborsClassifier(), knn_parameters),
+               ('CART', DecisionTreeClassifier(), cart_parameters),
+               ('RF', RandomForestClassifier(), rf_parameters),
+               ('XGBOOST', XGBClassifier(use_label_encoder=False, eval_metric='logloss'), xgboost_parameters),
+               ('LightGBM', LGBMClassifier(), lightgbm_parameters)]
+
+def hyperparameter_optimization(X, y, cv=3, scoring="roc_auc"):
+    """
+
+    Perform hyperparameter optimization for a list of classifiers.
+    Parameters:
+    -----------
+    X : array-like of shape (n_samples, n_features)
+        The input data.
+
+    y : array-like of shape (n_samples,)
+        The target labels.
+
+    cv : int, default=3
+        Number of folds for cross-validation.
+
+    scoring : str, default="roc_auc"
+        The scoring metric for evaluation.
+
+    Returns:
+    --------
+    best_models : dict
+        A dictionary containing the best models for each classifier.
+
+    """
+    best_models = {}
+    for name, classifier, params in classifiers:
+        print(f"~~~~~~~~~~~~~~~{name}~~~~~~~~~~~~~~~~~~")
+        cv_results = cross_validate(classifier, X, y, cv=cv, scoring=scoring)
+        print(f"{scoring} (Before): {round(cv_results['test_score'].mean(), 4)}")
+        gs_best = GridSearchCV(classifier, params, cv=cv, n_jobs=-1, verbose=False).fit(X, y)
+        final_model = classifier.set_params(**gs_best.best_params_)
+        cv_results = cross_validate(final_model, X, y, cv=cv, scoring=scoring)
+        print(f"{scoring} (After): {round(cv_results['test_score'].mean(), 4)}")
+        print(f"{name} best params: {gs_best.best_params_}", end="\n\n")
+        best_models[name] = final_model
+    return best_models
+
+best_models = hyperparameter_optimization(X, y)
+
+#############################################
+# 5. Stacking & Ensemble Learning
+############################################
+
+def voting_classifier(best_models, X, y):
+    """
+    Creates a voting classifier using the best models obtained from hyperparameter optimization.
+
+    Parameters:
+    -----------
+    best_models : dict
+        A dictionary containing the best models for each classifier.
+
+    X : array-like of shape (n_samples, n_features)
+        The input data.
+
+    y : array-like of shape (n_samples,)
+        The target labels.
+
+    Returns:
+    --------
+    voting_clf : VotingClassifier
+        A voting classifier fitted on the input data.
+
+    """
+    print("Voting Classifier...")
+
+    voting_clf = VotingClassifier(estimators=[('KNN', best_models["KNN"]),
+                                              ('RF', best_models["RF"]),
+                                              ('LightGBM', best_models["LightGBM"])],
+                                  voting='soft').fit(X, y)
+
+    cv_results = cross_validate(voting_clf, X, y, cv=3, scoring=["accuracy", "f1", "roc_auc"])
+    print(f"Accuracy: {cv_results['test_accuracy'].mean()}")
+    print(f"F1Score: {cv_results['test_f1'].mean()}")
+    print(f"ROC_AUC: {cv_results['test_roc_auc'].mean()}")
+    return voting_clf
+
+voting_clf = voting_classifier(best_models, X, y)
+
+######################################################
+# 6. Prediction for a New Observation
+######################################################
+
+X.columns
+random_user = X.sample(1, random_state=45)
+voting_clf.predict(random_user)
+
+joblib.dump(voting_clf, "voting_clf2.pkl")
+
+new_model = joblib.load("voting_clf2.pkl")
+new_model.predict(random_user)
